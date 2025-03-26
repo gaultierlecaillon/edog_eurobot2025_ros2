06697dd4ca86069a2290a22a3ed0b6d6537c376e
gaultierlecaillon <gaultier.lecail@gmail.com>
2025-03-26 20:11:44 +0000

    Clean actuator_package

--- README.md
+++ README.md
@@ -15,73 +15,23 @@
 
 ## Getting Started
 
-## ROS2 Cheat Sheet
-
-Create a python package
+Build the entire codebase
 ```shell
-ros2 pkg create hardware_package --build-type ament_python --dependencies rclpy
+colcon build
 ```
 
 Build a specific package
 ```shell
-colcon build --packages-select hardware_package
-colcon build --allow-overriding actuator_package --packages-select actuator_package 
-
+colcon build --packages-select actuator_package
 ```
 
-Build a specific package with symlink
+
+Start the lidar (mandatory if motion)
 ```shell
-colcon build --packages-select hardware_package --symlink-install
+lidar
 ```
-FUlll
+
+Launch a strategy
 ```shell
-colcon build --allow-overriding actuator_package --packages-select actuator_package --symlink-install
-```
-
-
-Create a Node
-
-Node are created in ~/ros2_ws/src/hardware_package/hardware_package/ folder
-```shell
-touche reset1_node
-```
-
-Python Node exemple with switch logic input:
-```python#!/usr/bin/env python3
-import rclpy
-from rclpy.node import Node
-import RPi.GPIO as GPIO
-import time
-import board
-
-
-class Reset1Node(Node):
-
-    def __init__(self):
-        super().__init__("reset1_node")
-
-        # Pi setup
-        self.tiretteGPIO = 14
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.tiretteGPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-        # ros2 setup
-        self.get_logger().info("Node reset1_node starting")
-        self.create_timer(0.25, self.timer_callback)
-
-    def timer_callback(self):
-        reset1_state = not GPIO.input(self.tiretteGPIO)
-        self.get_logger().info("Reset1:" + str(reset1_state))
-
-
-def main(args=None):
-    rclpy.init(args=args)
-    node = Reset1Node()
-    rclpy.spin(node)
-    rclpy.shutdown()
-
-
-if __name__ == "__main__":
-    main()
-
+launch strat:=test_strat
 ```
--- src/actuator_package/actuator_package/actuator_service.py
+++ src/actuator_package/actuator_package/actuator_service.py
@@ -3,9 +3,7 @@ import os
 import time
 import json
 import rclpy
-import subprocess
 from rclpy.node import Node
-from functools import partial
 import RPi.GPIO as GPIO
 from std_msgs.msg import Bool
 from robot_interfaces.srv import CmdActuatorService
@@ -15,15 +13,11 @@ from robot_interfaces.srv import CmdPositionService
 from example_interfaces.msg import String
 from std_msgs.msg import Int32
 
-
-
 # Stepper (set first BCM)
 from RpiMotorLib import RpiMotorLib
 
 # Servo
 from adafruit_servokit import ServoKit
-
-
 class ActuatorService(Node):
     # Stepper Config
     direction = 22  # Direction (DIR) GPIO Pin
@@ -32,10 +26,7 @@ class ActuatorService(Node):
 
     # Node State
     actuator_config = None
-    elevator_position = 0
-    grabber_bottom_loaded = False
-    grabber_top_loaded = False
-    
+    elevator_position = 0    
 
     def __init__(self):
         super().__init__("actuator_service")
@@ -59,42 +50,11 @@ class ActuatorService(Node):
         if not hasattr(self, 'voice_publisher'):
             self.voice_publisher = self.create_publisher(String, "voice_topic", 10)
         
-        ''' Services '''
-        
-        self.create_service(
-            CmdActuatorService,
-            "cmd_elevator_service",
-            self.elevator_callback)
-        
-        self.create_service(
-            CmdActuatorService,
-            "pami_service",
-            self.pami_callback)
-
-        self.create_service(
-            CmdActuatorService,
-            "jardiniere_service",
-            self.jardiniere_callback)
-        
-        self.create_service(
-            CmdActuatorService,
-            "cmd_arm_service",
-            self.arm_callback)
-        
+        ''' Services '''        
         self.create_service(
             CmdActuatorService,
             "cmd_grab_service",
             self.grab_callback)
-        
-        self.create_service(
-            CmdActuatorService,
-            "cmd_depose_top_service",
-            self.depose_top_callback)
-        
-        self.create_service(
-            CmdActuatorService,
-            "cmd_pince_service",
-            self.pince_callback)
 
         self.publish_pid()
         self.get_logger().info("Pince Service has been started.")
@@ -105,212 +65,25 @@ class ActuatorService(Node):
         #self.get_logger().error(f'\033[91m[publish_pid] {msg.data}\033[0m')
         self.pid_publisher.publish(msg)
 
-    def jardiniere_callback(self, request, response):
-        try:
-            step = self.actuator_config['elevator']['jardiniere']
-            self.move_elevator(step)
-            time.sleep(1)
-            self.kit.servo[3].angle = self.actuator_config['graber']['motor3']['open']
-            time.sleep(1)
-
-            self.cmd_forward(-175, 'emergency')
-
-            response.success = True
-        except Exception as e:
-            self.get_logger().error(f"Failed to execute jardiniere_callback: {e}")
-            response.success = False
-
-        return response
-
-    def pami_callback(self, request, response):
-        host = "192.168.0.103"
-        user = "edog"
-        password = "1pass4u!"
-        script_path = "/home/edog/edog-server/server.py"
-
-        # Command to run the script on the remote machine in the background
-        command = f"ssh {user}@{host} 'nohup python3 {script_path} > /dev/null 2>&1 &'"
-
-        # Use subprocess to run the command
-        try:
-            result = subprocess.run(
-                command,
-                shell=True,
-                text=True,
-                input=password,  # Be aware that inputting password directly can pose a security risk
-                capture_output=True
-            )
-            print("Script executed successfully:", result)
-        except Exception as e:
-            print("Error executing the script:", str(e))
-
-
-    def elevator_callback(self, request, response):
-        try:
-            self.get_logger().info(f"Elevator_callback Called : {request.param}")
-            if request.param == "loop":
-                for i in range(1):
-                    self.down_elevator()
-                    time.sleep(1)
-                    self.up_elevator()
-                    time.sleep(1)
-                    self.down_elevator()
-                    GPIO.output(self.EN_pin, GPIO.HIGH)
-            else:
-                self.get_logger().info(f"\033[91mself.get_logger().info(f\"Unknown action: {request.param} (elevator_callback)\")\033[0m")
-                
-
-            response.success = True
-        except Exception as e:
-            self.get_logger().error(f"Failed to execute elevator_callback: {e}")
-            response.success = False
-        return response
-    
-    def arm_callback(self, request, response):
-        try:
-            self.get_logger().info(f"Execute arm_callback with parm: {request.param}")
-
-            if request.param == "open":
-                self.kit.servo[4].angle = self.actuator_config['solarpanel']['motor4']['open']
-                step = self.actuator_config['elevator']['jardiniere']
-                self.move_elevator(step)
-            elif request.param == "close":
-                self.kit.servo[4].angle = self.actuator_config['solarpanel']['motor4']['close']
-                time.sleep(0.25)
-            response.success = True
-            
-        except Exception as e:
-            self.get_logger().error(f"Failed to execute grab_callback: {e}")
-            response.success = False
-        return response
     
     def grab_callback(self, request, response):
         self.get_logger().info(f"grab_callback Called : param={request.param}")
 
         try:
-            if not self.grabber_bottom_loaded and not self.grabber_top_loaded: # No plants stacked in the robot
-                self.kit.servo[3].angle = self.actuator_config['graber']['motor3']['open']
-                time.sleep(0.1)
-                self.kit.servo[2].angle = self.actuator_config['graber']['motor2']['open']
-                step = self.actuator_config['elevator']['down']
-                self.move_elevator(step)
-                time.sleep(0.2)
-                
-                self.cmd_forward(400, 'slow', False)
-                time.sleep(2.5)
-                              
-                self.kit.servo[2].angle = self.actuator_config['graber']['motor2']['plant'] #top
-                time.sleep(0.2)
-                self.kit.servo[3].angle = self.actuator_config['graber']['motor3']['semi-close'] # bottom
-                self.grabber_top_loaded = True
-                response.success = True
-            elif not self.grabber_bottom_loaded and self.grabber_top_loaded: # 1 stack of plants loaded
-                self.kit.servo[3].angle = self.actuator_config['graber']['motor3']['open'] - 4 # bottom
-                step = self.actuator_config['elevator']['plant']
-                self.move_elevator(step)
-                time.sleep(0.1)
-                self.cmd_forward(400, 'slow', False)
-                time.sleep(2.5)
-                self.kit.servo[3].angle = self.actuator_config['graber']['motor3']['plant'] # bottom
-                time.sleep(0.1)
-                step = self.actuator_config['elevator']['up']
-                self.move_elevator(step)
-                time.sleep(0.2)                
-                self.grabber_bottom_loaded = True                
-                response.success = True
-            elif self.grabber_bottom_loaded and self.grabber_top_loaded: # Robot already full
-                self.get_logger().error(f"Robot full, can't grab more plants 🙅")
-                response.success = False
-
+            step = self.actuator_config['elevator']['down']
+            self.move_elevator(step)
+            time.sleep(3)
+            
+            step = self.actuator_config['elevator']['infinite']
+            self.move_elevator(step)
+            time.sleep(4)
+            
+            response.success = True
+            
+            return response
         except Exception as e:
             self.get_logger().error(f"Failed to execute grab_callback: {e}")
             response.success = False
-
-        return response
-
-    
-    
-    def depose_top_callback(self, request, response):
-        self.get_logger().info(f"depose_top_callback Called : received={request.param}")
-        try:         
-            if self.grabber_bottom_loaded:  # both loaded => drop bottom
-                step = self.actuator_config['elevator']['depose']
-                self.move_elevator(step)
-                self.kit.servo[3].angle = self.actuator_config['graber']['motor3']['open']
-                time.sleep(0.5)                
-                self.cmd_forward(-150, 'slow', False)
-                time.sleep(2)
-                #self.kit.servo[3].angle = self.actuator_config['graber']['motor3']['semi-close']
-                #time.sleep(0.5) 
-                self.grabber_bottom_loaded = False
-                
-            elif not self.grabber_bottom_loaded and self.grabber_top_loaded: # only top loaded => drop both                
-                self.kit.servo[3].angle = self.actuator_config['graber']['motor3']['open']
-                step = self.actuator_config['elevator']['down']
-                self.move_elevator(step)
-                self.kit.servo[2].angle = self.actuator_config['graber']['motor2']['open']
-                time.sleep(1) 
-                               
-                self.cmd_forward(-200, 'slow', False)
-                time.sleep(2)
-                self.kit.servo[2].angle = self.actuator_config['graber']['motor2']['close']
-                time.sleep(0.1) 
-                self.kit.servo[3].angle = self.actuator_config['graber']['motor3']['close']
-                
-                self.grabber_top_loaded = False
-                self.grabber_bottom_loaded = False
-            
-            response.success = True
-        except Exception as e:
-            self.get_logger().error(f"Failed to execute depose_top_callback: {e}")
-            response.success = False
-
-        return response
-
-
-    def close_pince(self):
-        min_height = 180
-        isSafeToMovePince = self.elevator_position >= min_height
-        current_elevator_position = self.elevator_position
-        
-        if not isSafeToMovePince:            
-            self.move_elevator(min_height)   
-            time.sleep(0.3)         
-            
-        self.kit.servo[0].angle = self.actuator_config['pince']['motor0']['close']
-        self.kit.servo[1].angle = self.actuator_config['pince']['motor1']['close']
-        
-        if not isSafeToMovePince:
-            time.sleep(0.3)
-            self.move_elevator(current_elevator_position)   
-             
-            
-    def pince_callback(self, request, response):
-        
-        self.get_logger().info(f"pince_callback: {request}")
- 
-        response.success = False
-        try:
-            param = [str(x) for x in request.param.split(",")]
-            if param[0] == "right":
-                self.kit.servo[0].angle = self.actuator_config['pince']['motor0'][param[1]]
-                response.success = True
-            elif param[0] == "left":
-                self.kit.servo[1].angle = self.actuator_config['pince']['motor1'][param[1]]
-                response.success = True
-        except Exception as e:
-            self.get_logger().error(f"Failed to execute pince_callback: {e}")
-
-        return response
-
-
-    def down_elevator(self):
-        step = self.actuator_config['elevator']['down']
-        self.move_elevator(step)
-
-    def up_elevator(self):
-        step = self.actuator_config['elevator']['up']
-        self.move_elevator(step)
         
     def move_elevator(self, step):
         GPIO.output(self.EN_pin, GPIO.LOW)
@@ -325,7 +98,7 @@ class ActuatorService(Node):
                                     .05)  # initial delay [sec]
         self.elevator_position = step       
 
-    ''' Motion Funcitons '''
+    ''' Motion Functions '''
     def cmd_forward(self, distance_mm, mode='normal', evitement=True):
         service_name = "cmd_forward_service"
 
@@ -342,12 +115,6 @@ class ActuatorService(Node):
         client.call_async(request)
 
         self.get_logger().info(f"[Publish] {request} to {service_name}")
-
-    def speak(self, action):
-        msg = String()
-        msg.data = str(action)
-        self.voice_publisher.publish(msg)
-        #self.get_logger().info(f"[Publish topic] voice_topic msg:{msg}")
 
     ''' EndMotion Functions '''          
     
@@ -378,9 +145,10 @@ class ActuatorService(Node):
     def shutdown_callback(self, msg):       
         if msg.data:                  
             try:
-                self.get_logger().info("Shutdown signal received {msg}. Shutting down node... ") 
+                self.get_logger().info(f"Shutdown signal received {msg}. Shutting down node... ")
                 #self.initServo()                
                 self.initStepper()  
+                time.sleep(0.5)
                 GPIO.cleanup()
             except Exception as e:
                 self.get_logger().error(f"Error during shutdown: {e}")
